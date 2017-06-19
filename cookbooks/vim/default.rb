60693471b1735b3df98de0cb736b01794877163a
obiyuta <obi.yuta@gmail.com>
2017-06-19 19:49:00 +0900

    Install neobundle

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -1,2 +1,19 @@
 ln '.vim'
 ln '.vimrc'
+
+bundle_dir = File.join(ENV['HOME'], '.vim/bundle')
+
+directory bundle_dir do
+  action :create
+end
+
+git 'Pull latest neobundle' do
+  destination bundle_dir
+  repository 'git@github.com:Shougo/neobundle.vim.git'
+  only_if "test -e #{bundle_dir}"
+end
+
+install_sh = File.join(bundle_dir, 'bin/install.sh')
+execute 'Install NeoBundle' do
+  command install_sh  
+end
