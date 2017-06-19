b830284639b1c58fcf587ca4d1aa1f3f860bed95
obiyuta <obi.yuta@gmail.com>
2017-06-19 20:09:51 +0900

    Install ruby with rbenv

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,1 +1,13 @@
 ln '.gemrc'
+ln '.ruby-version'
+
+version = File.read(File.join(ENV['HOME'], '.ruby-version'))
+
+execute 'Install ruby' do
+  command "rbenv install #{version}"
+  only_if "rbenv versions | grep ${version}"
+end
+
+execute 'Set ruby version' do
+  command "rbenv global #{version}"
+end
