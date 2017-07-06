b3e5e2cf005d3b60ed91bf7473a2607177c9dd3b
obiyuta <obi.yuta@gmail.com>
2017-07-06 15:53:41 +0900

    Replace dokcer-sync spec

--- spec/localhost/docker_sync_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe command('command -v "docker-sync"') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match '/.rbenv/' }
-end
--- spec/localhost/ruby_spec.rb
+++ spec/localhost/ruby_spec.rb
@@ -30,3 +30,8 @@ end
 describe command('rbenv global') do
   its(:stdout) { should match version }
 end
+
+describe command('command -v "docker-sync"') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match '/.rbenv/' }
+end
